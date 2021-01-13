7f38414289f78d1b89537706aae3024657ed4bff
Saurabh Jadhav <Saurabh.Jadhav@genmills.com>
2021-01-14 00:18:34 +0530

    InitialCommit

---
+++ shopping-cart-demo/src/parts/PartInfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{description}}</h1>
+    </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+export default {
+    name:'PartInfo',   
+    props:['partType','id'],
+    computed:{
+        description(){ 
+            const {partType,id} = this;            
+            return "You have selected "+ partType + " with Index " +id;
+        },
+    },
+};
+</script>
